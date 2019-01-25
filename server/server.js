3ba2367b5d1c4c90478506469836c777602acb99
sirStephen <itobitoast@gmail.com>
2019-01-25 13:04:30 +0100

    feature(endpoint): create party
    endpoint to create parties
    
    [feature #163477785]

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import PartyController from './controllers/PartyController';
+
 dotenv.config();
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.status(200));
+
+app.post('/api/v1/party', PartyController.createParty);
 
 app.listen(process.env.PORT, () => {
   console.log(`app is listening on ${process.env.PORT}`);
