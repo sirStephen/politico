c35eb2004ea3a15ed3aedf549cc59604560a0d35
sirStephen <itobitoast@gmail.com>
2019-01-28 04:27:16 +0100

    feature(endpoint): get all parties
    request to get all parties
    
    [Feature #163507370]

--- server/test/test.js
+++ server/test/test.js
@@ -7,10 +7,10 @@ import { admin, user } from './testToken';
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('CREATE PARTY', () => {
-  it('admin should be able to create new party on POST /api/v1/party', (done) => {
+describe('PARTY', () => {
+  it('admin should be able to create new party on POST /api/v1/parties', (done) => {
     chai.request(app)
-      .post('/api/v1/party')
+      .post('/api/v1/parties')
       .set('Authorization', admin)
       .send({ partyName: 'pdp', hqAddress: '3, Wegbo Street, Lagos.' })
       .end((error, response) => {
@@ -24,9 +24,9 @@ describe('CREATE PARTY', () => {
       });
   });
 
-  it('user should not be able to create new party on POST /api/v1/party', (done) => {
+  it('user should not be able to create new party on POST /api/v1/parties', (done) => {
     chai.request(app)
-      .post('/api/v1/party')
+      .post('/api/v1/parties')
       .set('Authorization', user)
       .send({ partyName: 'pdp', hqAddress: '3, Wegbo Street, Lagos.' })
       .end((error, response) => {
@@ -34,6 +34,18 @@ describe('CREATE PARTY', () => {
         response.should.have.status(401);
         response.body.should.be.a('object');
         response.body.should.have.property('message').eql('you are not authorized');
+        done();
+      });
+  });
+
+  it('user/admin should be able to get all party on GET /api/v1/parties', (done) => {
+    chai.request(app)
+      .get('/api/v1/parties')
+      .end((error, response) => {
+        console.log(response.body);
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('data');
         done();
       });
   });
