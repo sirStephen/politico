6f3ba0c24ce78f86277a35599b6ce52fda8f5718
sirStephen <itobitoast@gmail.com>
2019-02-07 12:39:45 +0100

    made change to db_configuration

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -1,39 +1,20 @@
 import { config } from 'dotenv';
-import url from 'url';
 
 config();
+const dbConfig = {};
 
+dbConfig.test = {
+  user: process.env.TEST_DB_USER,
+  host: process.env.TEST_DB_HOST,
+  database: process.env.TEST_DB_NAME,
+  port: process.env.TEST_DB_PORT,
+};
 
-// const dbConfig = {};
-
-// dbConfig.test = {
-//   user: process.env.TEST_DB_USER,
-//   host: process.env.TEST_DB_HOST,
-//   database: process.env.TEST_DB_NAME,
-//   port: process.env.TEST_DB_PORT,
-// };
-
-// dbConfig.local = {
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-// };
-
-
-// export default dbConfig;
-// const Pool = require('pg-pool');
-
-
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
-
-const dbConfig = {
-  user: auth[0],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  ssl: true,
+dbConfig.local = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 };
 
 export default dbConfig;
--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 import dbConfig from '../../secrets/db_configuration';
 
 config();
-const pool = (process.env.NODE_ENV === 'test') ? new Pool(dbConfig.test) : new Pool(dbConfig);
+const pool = (process.env.NODE_ENV === 'test') ? new Pool(dbConfig.test) : new Pool(dbConfig.local);
 
 console.log('===========================', pool);
 console.log('============================', process.env.NODE_ENV);
