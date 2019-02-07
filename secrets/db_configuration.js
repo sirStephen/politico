511e8e8ebc64c49fbb56c78daacfcf310a1f35cf
sirStephen <itobitoast@gmail.com>
2019-02-07 11:16:05 +0100

    remove secrets folder

---
+++ secrets/db_configuration.js
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+
+config();
+const dbConfig = {};
+
+dbConfig.test = {
+  user: process.env.TEST_DB_USER,
+  host: process.env.TEST_DB_HOST,
+  database: process.env.TEST_DB_NAME,
+  port: process.env.TEST_DB_PORT,
+};
+
+dbConfig.local = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
+
+
+export default dbConfig;
