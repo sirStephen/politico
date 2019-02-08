ee1986a7997136842613a685e1f733452d35ccd8
sirStephen <itobitoast@gmail.com>
2019-02-08 07:38:42 +0100

    create database on travis

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -1,18 +1,21 @@
-import { config } from 'dotenv';
+// import { config } from 'dotenv';
 
-config();
-const dbConfig = {};
+// config();
+// const dbConfig = {};
 
-dbConfig.test = {
-  user: process.env.TEST_DB_USER,
-  host: process.env.TEST_DB_HOST,
-  database: process.env.TEST_DB_NAME,
-  port: process.env.TEST_DB_PORT,
-};
+// dbConfig.test = {
+//   user: process.env.TEST_DB_USER,
+//   host: process.env.TEST_DB_HOST,
+//   database: process.env.TEST_DB_NAME,
+//   port: process.env.TEST_DB_PORT,
+// };
 
-// export const DATABASE_URL = 'postgres://postgres@localhost:5432/politicaldb',
+// dbConfig.local = {
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// };
 
-
-// // export default dbConfig;
-
-export default config;
+// export default dbConfig;
